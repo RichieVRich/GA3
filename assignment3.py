22628c22da98e9291ea269776d3b6b781a14963a
unknown <villagri@oregonstate.edu>
2020-11-17 21:39:31 -0800

    Added Input Compatiability

--- assignment3.py
+++ assignment3.py
@@ -6,6 +6,7 @@
 
     Also, I will use <python3> to run this code.
 '''
+import math
 
 def vidrach_itky_leda(input_file_path, output_file_path):
     '''
@@ -16,16 +17,53 @@ def vidrach_itky_leda(input_file_path, output_file_path):
         input_file_path (str): full path of the input file.
         output_file_path (str): full path of the output file.
     '''
+    global Array
+    global n
+    global line
+    global count
+    global i
+    global insert
+    global current
+    global rows
+    global columns
+    rows = 0
+    columns = 0
+    Array = []
+    current = []
+    global maximum
+    maximum = 0
+    global x
+    count = 0
+    n = 0
+    fp = open(input_file_path)
+    #print('hello')
+    line = fp.readline()
+    n = int(math.sqrt(int(line)))
+    print(line)
+    print(n)
+    print("n is", n )
+    columns, rows = (n, n)
+    for x in range(n):
+        if x == line:
+            break
+      #  print(line[0])
+        line = fp.readline()
+        line = line.rstrip('\n')
+        line = line.split(",")
+
+        print(line)
+        line = [int(i) for i in line]
+        Array.append(line)
     # 2D test array
-    test_array = [[1,2,3],[4,5,6],[3,2,1]]
+
     # Print 2d array
-    for r in test_array:
+    for r in Array:
         for c in r:
             print(c, end = " ")
         print()
-    n = len(test_array) - 1
-    red = test_array[0][0]
-    blue = test_array[n][n]
+    n = len(Array) - 1
+    red = Array[0][0]
+    blue = Array[n][n]
     # Red X and Y
     r_x = 0
     r_y = 0
@@ -35,6 +73,8 @@ def vidrach_itky_leda(input_file_path, output_file_path):
     # Tokens for valid movement
     move1 = 0
     move2 = 0
+    move3 = 0
+    move4 = 0
     # Bound Checker Tokens
     bounds1 = 0
     bounds2 = 0
@@ -61,13 +101,13 @@ def vidrach_itky_leda(input_file_path, output_file_path):
            # print("blue is bigger = ", blue)
             if r_y + blue <= n:
                 # Check bottom Y
-                move1 = test_array[r_y + blue][r_x]
+                move1 = Array[r_y + blue][r_x]
             else:
             #    print("failed out of bounds move1", r_y + blue)
                 fail1 = 1
             if r_x + blue <= n:
                 # Check Right X
-                move2 = test_array[r_y][r_x + blue]
+                move2 = Array[r_y][r_x + blue]
             else:
            #     print("failed out of bounds move2 ", r_x + blue)
                 fail2 = 1
@@ -77,10 +117,10 @@ def vidrach_itky_leda(input_file_path, output_file_path):
             if compb == 0:
                 bounds1 = b_y - move1
                 bounds2 = b_x - move2
-                # print("by ", bounds1)
-                # print("bx ", bounds2)
+                print("by ", bounds1)
+                print("bx ", bounds2)
                 if bounds1 < 0:
-                    # print(" b1 false")
+                    print(" b1 false")
                     bounds1 = 99
                 if bounds2 < 0:
                     # print(" b2 false")
@@ -88,7 +128,7 @@ def vidrach_itky_leda(input_file_path, output_file_path):
                 # Smallest Wins in this situtation
                 if bounds1 < bounds2 or r_x == n:
                     if fail1 == 0:
-                        #      print("didnt fail")
+                        print("didnt fail")
                         red = move1
                         r_y = r_y + blue
                 elif fail2 == 0:
@@ -112,14 +152,14 @@ def vidrach_itky_leda(input_file_path, output_file_path):
            # print("red is bigger")
             if b_y - red >= 0:
                 # Check bottom Y
-                move1 = test_array[b_y - red][b_x]
+                move1 = Array[b_y - red][b_x]
                 #print(move1)
             else:
                 fail1 = 1
                 #print("failed out of bounds move1", b_y - red)
             if b_x - red >= 0:
                 # Check Right X
-                move2 = test_array[b_y][b_x - red]
+                move2 = Array[b_y][b_x - red]
                # print(move2)
             else:
                # print("failed out of bounds move")
