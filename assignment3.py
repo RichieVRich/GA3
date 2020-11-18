718c38f5e79c241ac89993f477c7e7d2e5189997
unknown <villagri@oregonstate.edu>
2020-11-17 19:42:41 -0800

    Changes to logic

--- assignment3.py
+++ assignment3.py
@@ -17,7 +17,7 @@ def vidrach_itky_leda(input_file_path, output_file_path):
         output_file_path (str): full path of the output file.
     '''
     # 2D test array
-    test_array = [[1,2,4,3],[3,4,1,2],[3,1,2,3],[2,3,1,2]]
+    test_array = [[1,2,3],[4,5,6],[3,2,1]]
     # Print 2d array
     for r in test_array:
         for c in r:
@@ -38,97 +38,142 @@ def vidrach_itky_leda(input_file_path, output_file_path):
     # Bound Checker Tokens
     bounds1 = 0
     bounds2 = 0
-
-    print("red = ", red," blue = ", blue)
     completed = 0
     switch = -1
-    print("Red = ", red, " Blue = ", blue)
-    print("position red x|y", r_x, " | ", r_y)
-    print("position blue x|y", b_x, " | ", b_y)
+ #   print("Red = ", red, " Blue = ", blue)
+ #   print("position red x|y", r_x, " | ", r_y)
+ #   print("position blue x|y", b_x, " | ", b_y)
+    if red < blue:
+        switch = 0
+    else:
+        switch = 1
+    # Start the code
+    compr = 0
+    compb = 0
     while completed < 1:
-        if red < blue or switch == 0:
+        # Moving Red
+        if switch == 0:
+            if r_y == n and r_x == n:
+                compr = 1
             switch = 1
-            print("blue is bigger = ", blue)
+            fail1 = 0
+            fail2 = 0
+           # print("blue is bigger = ", blue)
             if r_y + blue <= n:
                 # Check bottom Y
-
                 move1 = test_array[r_y + blue][r_x]
             else:
-                print("failed out of bounds move1")
-                move1 = -1
+            #    print("failed out of bounds move1", r_y + blue)
+                fail1 = 1
             if r_x + blue <= n:
                 # Check Right X
                 move2 = test_array[r_y][r_x + blue]
             else:
-                print("failed out of bounds move2")
-                move2 = 0
-
+           #     print("failed out of bounds move2 ", r_x + blue)
+                fail2 = 1
             # Check for valid input
-
-            print("by ", b_y , "move1 ", move1)
-            print("bx ", b_x, "move2 ",move2)
-            bounds1 = b_y - move1
-            bounds2 = b_x - move2
-            print("by ", bounds1)
-            print("bx ", bounds2)
-            if bounds1 < 0 :
-                print(" b1 false")
-                bounds1 = 99
-            if bounds2 < 0:
-                print(" b2 false")
-                bounds2 = 99
-            # Smallest Wins in this situtation
-            if bounds1 < bounds2 or r_x == n:
-                red = move1
-                r_y = r_y + blue
+           # print("by ", b_y , "move1 ", move1)
+           # print("bx ", b_x, "move2 ",move2)
+            if compb == 0:
+                bounds1 = b_y - move1
+                bounds2 = b_x - move2
+                # print("by ", bounds1)
+                # print("bx ", bounds2)
+                if bounds1 < 0:
+                    # print(" b1 false")
+                    bounds1 = 99
+                if bounds2 < 0:
+                    # print(" b2 false")
+                    bounds2 = 99
+                # Smallest Wins in this situtation
+                if bounds1 < bounds2 or r_x == n:
+                    if fail1 == 0:
+                        #      print("didnt fail")
+                        red = move1
+                        r_y = r_y + blue
+                elif fail2 == 0:
+                    #   print("didn't fail")
+                    red = move2
+                    r_x = r_x + blue
+                else:
+                    #    print("cannot reasonably move switch to the other")
+                    switch = 1
             else:
-                red = move2
-                r_x = r_x + blue
-            print("red = ", red)
-
-
-        elif blue < red or switch == 1:
+                if r_x == n:
+                    r_y = r_y + blue
+                else:
+                    r_x = r_x + blue
+           # print("red = ", red)
+        # Moving Blue ===========
+        elif switch == 1:
             switch = 0
-            print("red is bigger")
-            # Check bottom Y
-            move1 = test_array[b_y - red][b_x]
-            print(move1)
-            # Check Right X
-            move2 = test_array[b_y][b_x - red]
-            print(move2)
-            # Check for valid input
-
-            bounds1 = r_y + move1
-            bounds2 = r_x + move2
-            print("by ", bounds1)
-            print("bx ", bounds2)
-            if bounds1 > 4 :
-                print(" b1 false")
-                bounds1 = -1
-            if bounds2 > 4:
-                print(" b2 false")
-                bounds2 = -1
-            # Smallest Wins in this situtation
-            if bounds2 > bounds1 or b_x == 0:
-                print("move1 wins")
-                blue  = move1
-                b_y = b_y - red
+            fail1 = 0
+            fail2 = 0
+           # print("red is bigger")
+            if b_y - red >= 0:
+                # Check bottom Y
+                move1 = test_array[b_y - red][b_x]
+                #print(move1)
             else:
-                print("move2 wins")
-                blue = move2
-                b_x = b_x - red
-            print(blue)
+                fail1 = 1
+                #print("failed out of bounds move1", b_y - red)
+            if b_x - red >= 0:
+                # Check Right X
+                move2 = test_array[b_y][b_x - red]
+               # print(move2)
+            else:
+               # print("failed out of bounds move")
+                fail2 = 1
+
+            # Check for valid input, only if r isn't completed
+            if compr == 0:
+                bounds1 = r_y + move1
+                bounds2 = r_x + move2
+              #  print("by ", bounds1)
+              #  print("bx ", bounds2)
+                if bounds1 > n:
+               #     print(" r1 false")
+                    fail1 = 1
+                    bounds1 = -1
+                if bounds2 > n:
+                #    print(" r2 false")
+                    fail2 = 1
+                    bounds2 = -1
+                # Smallest Wins in this situtation
+                if bounds2 < bounds1 or b_x == 0:
+                   # print("changing bound 1 ")
+                    if fail1 == 0:
+                    #    print("move1 wins")
+                        blue = move1
+                        b_y = b_y - red
+                elif fail2 == 0:
+                   # print("chainging boutn 2")
+                   # print("move2 wins")
+                    blue = move2
+                    b_x = b_x - red
+                else:
+                  #  print("cannot move token SWITCH!")
+                    switch = 0
+            else:
+                if b_x == 0:
+                    b_y = b_y - red
+                else:
+                    b_x = b_x - red
+
+          #  print(blue)
 
 
         else:
-            print("there is tie leave it to switch")
+         #   print("there is tie leave it to switch")
+            if switch < 0:
+                switch = 0
 
         if r_x == n and r_y == n and b_x == 0 and b_y == 0:
             completed = completed + 1
         print("Red = ", red, " Blue = ", blue)
-        print("position red x|y", r_x, " | ",r_y)
-        print("position blue x|y", b_x, " | ", b_y)
-print("test")
+        print("position red x: ", r_x, " | y:",r_y)
+        print("position blue x: ", b_x, " | y:", b_y)
+#print("test")
 
 pass
 
